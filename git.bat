0c9f6487cbc23078930a75c17fa9988ef36e2f18
tve3 <vladu4eg@mail.ru>
2021-04-14 18:43:30 +0300

    Initial commit

---
+++ git.bat
@@ -0,0 +1,3 @@
+mklink /J "D:\hide\game\hide\" "D:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota_addons\hide\"
+
+mklink /J "D:\hide\content\hide\" "D:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\content\dota_addons\hide\"
